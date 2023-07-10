0f5ec7cf8e0fcf79bba0a21662b1b96a69a04537
ibrahim <ullahibrahimkhalil3@gmail.com>
2023-07-09 23:14:22 -0400

    added all flask froms LoginForm, RegisterForm and ContactForm in forms.py python file

---
+++ forms.py
@@ -0,0 +1,40 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.fields import EmailField
+from wtforms.validators import InputRequired, EqualTo, Email, Length, Regexp, ValidationError
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Login')
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[InputRequired()])
+    email = EmailField('Email', validators=[InputRequired(), Email()])
+    message = TextAreaField('Message', validators=[InputRequired()], render_kw={'rows': 15 })
+    # phone = StringField('Phone number', validators=[InputRequired()])
+    submit = SubmitField('Send Message')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[InputRequired(),
+                                       Length(4, 64),
+                                       Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                              'Usernames must start with a letter and must have only letters, numbers, dots or underscores')])
+    email = EmailField('Email', validators=[InputRequired(), Email()])
+    phone = StringField('Phone number', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired(), Length(8)])
+    password2 = PasswordField('Repeat password',
+                              validators=[InputRequired(),
+                                          EqualTo('password',
+                                                  message='Passwords must match.')])
+    submit = SubmitField('Register')
+
+    def validate_password(self, field):
+        with open('data/common_passwords.txt') as f:
+            for line in f.readlines():
+                if field.data == line.strip():
+                    raise ValidationError('Your password is too common.')
